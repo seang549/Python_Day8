ae711054fcbd476a8b492093c5f0e368b2390419
Sean Guerrero <seanguerrero549@gmail.com>
2024-01-23 15:00:02 -0800

    Finished day 8

---
+++ script.py
@@ -0,0 +1,12 @@
+# def greet(name):
+#     print(f"hello {name}")
+#     print(f"hi {name}")
+#     print(f"hey {name}")
+
+# greet("Angela")
+
+def greet_with(name, location):
+    print(f"hi {name} from {location}")
+
+greet_with(location = "Sean", name = "California")
+greet_with(name = "Sean", location = "California")
